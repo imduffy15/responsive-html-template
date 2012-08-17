ef3106fc157eddd9c599f35042f7d1b2c8f733aa
Ian Duffy <ian@ianduffy.ie>
2012-08-17 18:31:59 +0100

    Move JS into external file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+$(window).load(function() {
+    $('#featured').flexslider({
+      start: function(slider){
+        $('#featured').removeClass('loading');
+      }
+    }
+  );            
+});
 
+$(document).load(function() {
+   $(".container").fitVids();
+})
